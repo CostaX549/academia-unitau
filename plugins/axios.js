57ca67a6ec001aa8080594d35b52ddf5f2bb91a0
CostaX549 <costasantos896@gmail.com>
2024-06-05 20:34:49 -0300

    Mudanças 05/06/2024 - 20:34

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default defineNuxtPlugin((NuxtApp) => {
-    axios.defaults.baseURL = 'http://192.168.0.72'
+    axios.defaults.baseURL = 'https://unitauacademia.rf.gd'
 
     axios.defaults.withCredentials = true;
     axios.defaults.withXSRFToken = true;
