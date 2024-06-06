d7b11c08f84029532307c41f357c04a413748eb1
CostaX549 <costasantos896@gmail.com>
2024-06-05 23:46:47 -0300

    Julio7

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default defineNuxtPlugin((NuxtApp) => {
-    axios.defaults.baseURL = 'https://unitauacademia.rf.gd'
+    axios.defaults.baseURL = 'https://academiaunitauapi.vercel.app/api'
 
     axios.defaults.withCredentials = true;
     axios.defaults.withXSRFToken = true;
